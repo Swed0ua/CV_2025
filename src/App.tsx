f13ae0cd03c4cc41c649b4e5be4fa2d95fbb6722
Swed0ua <shmagala2001@gmail.com>
2025-09-07 14:44:41 +0300

    fix(slide scroll)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <MainScreen />
+      {/* <iframe src='https://my.spline.design/digitalpass-NaEnQ17JYfnR13DFAK3rCZAy/' frameborder='0' width='100%' height='100%'></iframe> */}
     </div>
   );
 }
