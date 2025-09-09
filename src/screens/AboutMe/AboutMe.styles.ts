17a003ecdd3d9764671371c1a1f8f3682590fe04
Swed0ua <shmagala2001@gmail.com>
2025-09-09 13:07:12 +0300

    feat(update aboutMe design)

--- src/screens/AboutMe/AboutMe.styles.ts
+++ src/screens/AboutMe/AboutMe.styles.ts
@@ -3,13 +3,18 @@ import { CSSProperties } from 'react';
 export const aboutMeContainerStyles: CSSProperties = {
   width: '100%',
   height: '100vh',
-  backgroundColor: '#000000',
+  background:
+    'linear-gradient(-45deg, #22201b,rgb(63, 34, 34),rgb(22, 14, 63), #181212)',
+  backgroundSize: '400% 400%',
+  animation: 'gradientShift 35s ease infinite',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   color: 'white',
   fontFamily: 'Roboto Mono, monospace',
   padding: '20px',
+  position: 'relative',
+  overflow: 'hidden',
 };
 
 export const aboutMeContentStyles: CSSProperties = {
@@ -19,6 +24,7 @@ export const aboutMeContentStyles: CSSProperties = {
   flexDirection: 'column',
   alignItems: 'center',
   textAlign: 'center',
+  paddingTop: '100px',
 };
 
 export const aboutMeTitleStyles: CSSProperties = {
@@ -26,11 +32,16 @@ export const aboutMeTitleStyles: CSSProperties = {
   fontWeight: 'bold',
   marginBottom: '2rem',
   color: '#A5B4FF',
+  textShadow: '0 0 30px rgba(165, 180, 255, 0.2)',
+  position: 'relative',
+  zIndex: 3,
 };
 
 export const aboutMeTextStyles: CSSProperties = {
   fontSize: '1.2rem',
   lineHeight: '1.6',
-  opacity: 0.8,
+  opacity: 0.9,
   maxWidth: '800px',
+  position: 'relative',
+  padding: '2rem',
 };
