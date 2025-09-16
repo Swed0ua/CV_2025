bbbfe84d8843b183f87dd372654317a65b78543e
Swed0ua <shmagala2001@gmail.com>
2025-09-17 00:12:56 +0300

    feat(update styles for blobs in mainContent)

--- src/screens/MainContent/MainContent.tsx
+++ src/screens/MainContent/MainContent.tsx
@@ -5,11 +5,12 @@ import {
   mainContentTextStyles,
 } from './MainContent.styles';
 import ColorBlobs from '../../components/ColorBlobs/ColorBlobs';
+import { MAIN_CONTENT_BLOBS } from '../../constants/colorBlobsData';
 
 const MainContent: React.FC = () => {
   return (
     <div className="mainContentContainer" style={mainContentContainerStyles}>
-      <ColorBlobs />
+      <ColorBlobs blobs={MAIN_CONTENT_BLOBS} />
       <div className="mainContentText" style={mainContentTextStyles}>
         Hello World
       </div>
