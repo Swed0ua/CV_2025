e97a50c1a74f0ac5934d5ed6f4284460b8b5d892
Swed0ua <shmagala2001@gmail.com>
2025-10-08 01:31:51 +0300

    feat(added calc position in myServices)

--- src/components/MyServices/MyServices.tsx
+++ src/components/MyServices/MyServices.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import './MyServices.css';
+import { calculateScrollTransform } from '../../utils/math';
 
 export interface MyServicesProps {
   className?: string;
@@ -11,9 +12,42 @@ export const MyServices: React.FC<MyServicesProps> = ({
   className = '',
   style = {},
 }) => {
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  const [elemenWidthProc, setElementWidthProc] = useState(80);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!elementRef.current) return;
+
+      const vl = calculateScrollTransform({
+        element: elementRef.current,
+        endpoint: 0.35,
+        maxShift: 80,
+        minShift: 100,
+      });
+
+      setElementWidthProc(vl);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={`my-services ${className}`.trim()} style={style}>
-      <div className="mobile-service-card">
+    <div
+      className={`my-services ${className}`.trim()}
+      style={{ ...style, paddingBottom: 2000 }}
+    >
+      <div
+        className="mobile-service-card"
+        ref={elementRef}
+        style={{ width: `${elemenWidthProc}%` }}
+      >
         <h1>Hello World - My Services</h1>
       </div>
     </div>
