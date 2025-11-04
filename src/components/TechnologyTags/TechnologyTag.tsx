f06a0187a5d5fdea99391d1f65bb0000172b3a17
Swed0ua <shmagala2001@gmail.com>
2025-11-04 19:13:35 +0200

    feat(added params mainColor for TechTag)

--- src/components/TechnologyTags/TechnologyTag.tsx
+++ src/components/TechnologyTags/TechnologyTag.tsx
@@ -4,16 +4,22 @@ import './TechnologyTag.css';
 export interface TechnologyTagProps {
   technology: string;
   className?: string;
+  mainColor?: string;
   style?: React.CSSProperties;
 }
 
 export const TechnologyTag: React.FC<TechnologyTagProps> = ({
   technology,
   className = '',
+  // eslint-disable-next-line no-unused-vars
+  mainColor = '#a5b4ff',
   style,
 }) => {
   return (
-    <span className={`technology-tag ${className}`.trim()} style={style}>
+    <span
+      className={`technology-tag ${className}`.trim()}
+      style={{ ...style, '--main-color': mainColor } as React.CSSProperties}
+    >
       {technology}
     </span>
   );
